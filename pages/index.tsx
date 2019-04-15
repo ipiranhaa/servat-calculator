5bda1685a20aac80d32e2d38f85e2cb1048d0523
Tanate 'Pla' Meaksriswan <Tanate.M@kaidee.com>
2019-04-15 08:19:38 +0700

    Setup typescript

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import Layout from '../src/components/Layout'
+
+function Index() {
+  return <Layout>Welcome !</Layout>
+}
+
+export default Index
---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+type Props = {
+  children: any
+}
+
+export default ({ children }: Props) => (
+  <div>
+    <Head>
+      <title>Servat Calculator</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    {children}
+    <footer>{'Footer'}</footer>
+  </div>
+)
